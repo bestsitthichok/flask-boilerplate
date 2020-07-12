09172b7c47f3baa65849d56c35a76d90a1388419
bestsitthichok <khom14110@gmail.com>
2020-07-12 13:33:34 +0700

    add

--- app.py
+++ app.py
@@ -7,7 +7,27 @@ CORS(app)
 
 @app.route('/')
 def root():
-    return "Working"
+    return "hello my bestfriend"
+
+@app.route('/best')
+def new_one_function():
+    username = request.args.get('name')
+    print(username)
+    return 'hi ' + username
+
+@app.route('/sum')
+def my_sum():
+    a = request.args.get('a')
+    a = int(a)
+    b = int(request.args.get('b'))
+    answer = a + b
+    return str(answer)
+
+@app.route('/mybest')
+def mybest():
+    username = request.args.get('name')
+    return  render_template('home.html',name = username)
+
 
 if __name__ == "__main__":
     app.run(debug = True, host="0.0.0.0", port=5000)
