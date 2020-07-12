d0548c0a6aa4c2cf983d4a35a471736da4807fe2
bestsitthichok <khom14110@gmail.com>
2020-07-12 15:52:31 +0700

    add std

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
+import postgresql_api
+
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
@@ -28,6 +30,10 @@ def mybest():
     username = request.args.get('name')
     return  render_template('home.html',name = username)
 
+@app.route('/studentlist')
+def student_list():
+    stdlist = postgresql_api.get_student_data()
+    return  render_template('table.html',stdlist=stdlist)
 
 if __name__ == "__main__":
     app.run(debug = True, host="0.0.0.0", port=5000)
